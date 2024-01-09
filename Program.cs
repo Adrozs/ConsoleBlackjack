ae19372731616e20ab6289aadcc4ce146fd59a92
Adrozs <adrozsahegyi@gmail.com>
2024-01-09 11:16:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleBlackjack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
